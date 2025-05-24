09cd3ce627e3ab9ddca210134d215abf6ce27435
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-05-25 03:06:11 +0800

    Added the core methods of the code for quiz creator

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,11 @@
 class QuizCreator:
+    def __init__(self):
+    
+    def save_to_file(self):
+
+    def create_quiz(self):
+
+    def main_file(self):
 
 if __name__ == "__main__":
     creator = QuizCreator()
