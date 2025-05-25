2a56f1844a2c5d60eea9cb79bbbd96ad375d9fe0
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-05-25 11:59:20 +0800

    Added display_menu method to show options or the main menu to the user

--- quiz_main_program_launcher.py
+++ quiz_main_program_launcher.py
@@ -7,3 +7,12 @@ from quiz_game import QuizGame
 class MainMenu:
     def __init__(self):
         self.display_menu()
+
+    def display_menu(self):
+        while True:
+            print(colored("\n✨ Welcome to My Quiz Program ✨", "cyan", attrs=["bold"]))
+            print(colored("1. Create a New Quiz", "yellow"))
+            print(colored("2. Take a Quiz", "green"))
+            print(colored("3. Exit", "red"))
+
+            choice = input(colored("\nPlease select an option (1-3): ", "blue")).strip()
