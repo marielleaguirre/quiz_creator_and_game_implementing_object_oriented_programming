c14fc9fea989ce707024944a2e77d7e59a6e02a4
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-05-25 11:56:07 +0800

    Created a MainMenu class to handle the main interface logic

--- quiz_main_program_launcher.py
+++ quiz_main_program_launcher.py
@@ -3,3 +3,7 @@ import time
 from termcolor import colored
 from quiz_creator import QuizCreator 
 from quiz_game import QuizGame
+
+class MainMenu:
+    def __init__(self):
+        self.display_menu()
